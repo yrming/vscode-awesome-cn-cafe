7d8b4b5d6cf3d7c80525a677154f430541c7c0a1
YRM <yrm@outlook.com>
2022-05-16 19:34:03 +0800

    chore: add process data script

---
+++ scripts/process.ts
@@ -0,0 +1,47 @@
+import * as path from 'path'
+import { promises as fs } from 'fs'
+import * as fg from 'fast-glob'
+;(async () => {
+  const readme = await fs.readFile('./cafe/README.md')
+  const regex = /\* \[(.*)\s*\((\d+)\)\]\((.*).geojson\)/g
+  const data: Record<string, any> = {}
+
+  let match
+
+  // eslint-disable-next-line no-cond-assign
+  while ((match = regex.exec(readme.toString()))) {
+    data[match[3]] = {
+      name: match[1],
+      count: +match[2]
+    }
+  }
+
+  const files = await fg('./cafe/*.geojson')
+
+  for (const file of files) {
+    const name = path.parse(file).name
+    data[name].data = JSON.parse(await fs.readFile(file, 'utf-8'))
+
+    data[name].data.features.forEach((i: any) => {
+      i.properties.shortname = i.properties['名称']
+        .replace(/[(（].*[)）]/g, '')
+        .replace('星巴克', 'Starbucks')
+        .replace(/Starbucks\s*Starbucks/, 'Starbucks')
+        .trim()
+
+      i.properties.referrers = Object.keys(i.properties)
+        .map((i) => i.match(/(@[\w_-]+)/))
+        .filter(Boolean)
+        .map((i) => i![1])
+    })
+
+    // calc center
+    const coordinates = data[name].data.features.map((i: any) => i.geometry.coordinates)
+    data[name].center = coordinates
+      .reduce((a: any, b: any) => [a[0] + b[0], a[1] + b[1]], [0, 0])
+      .map((i: any) => i / coordinates.length)
+  }
+
+  await fs.writeFile('./src/data.json', JSON.stringify(data), 'utf-8')
+  await fs.writeFile('./src/build.ts', `export const BuildTime = ${+new Date()}\n`, 'utf-8')
+})()
